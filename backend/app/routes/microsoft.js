25f8155fe86d0fd0bb2bb1c1f34c562620388e3b
kuminin <kumin.in@hotmail.com>
2016-10-22 00:34:30 -0700

    Added Routes for users, collections, and passwords

---
+++ backend/app/routes/microsoft.js
@@ -0,0 +1,53 @@
+module.exports = function(app) {
+    app.get('/user/:id', (req, res) => {
+
+    });
+
+    app.post('/user/:id', (req, res) => {
+
+    });
+
+    app.put('/user/:id', (req, res) => {
+
+    });
+
+    app.delete('/user/:id', (req, res) => {
+
+    });
+
+    app.get('/user/:id/collection', (req, res) => {
+
+    });
+
+    app.get('/user/:id/collection/:name', (req, res) => {
+
+    });
+
+    app.post('/user/:id/collection/:name' (req, res) => {
+
+    });
+
+    app.put('/user/:id/collection/:name', (req, res) => {
+
+    });
+
+    app.delete('/user/:id/collection/:name', (req, res) => {
+
+    });
+
+    app.get('/user/:id/collection/:name/password', (req, res) => {
+
+    });
+
+    app.put('/user/:id/collection/:name/password', (req, res) => {
+
+    });
+
+    app.delete('/user/:id/collection/:name/password', (req, res) => {
+
+    });
+
+    app.post('/user/:id/collection/:name/password', (req, res) => {
+
+    });
+};
