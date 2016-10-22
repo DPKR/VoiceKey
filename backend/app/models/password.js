6efccf127b18cad3fabaff9941de0bb77c90d925
Paul Kim <Paul Kim>
2016-10-22 14:28:12 -0700

    Created Password Schema

---
+++ backend/app/models/password.js
@@ -0,0 +1,18 @@
+"use strict";
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    ObjectId = Schema.ObjectId;
+
+var passwordSchema = new Schema({
+    description: {
+      type: String,
+      maxlength: 200
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    hash: {type: String, required: true}
+});
+
+mongoose.model('Password', passwordSchema);
