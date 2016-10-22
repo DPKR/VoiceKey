29ba2d2f7e014044d5b22195b6999f49aba46b9f
kuminin <kumin.in@hotmail.com>
2016-10-21 23:55:01 -0700

    required microsoft.js

--- backend/app.js
+++ backend/app.js
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
-
+app.use('./app/routes/microsoft');
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
