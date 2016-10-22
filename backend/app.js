e729d11e303c5a1a048a1e8be6d77aa4ba925552
kuminin <kumin.in@hotmail.com>
2016-10-22 14:56:18 -0700

    removed unnecessary routes

--- backend/app.js
+++ backend/app.js
@@ -5,9 +5,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
 
 require('./app/models/db.js');
@@ -23,8 +20,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use('/', routes);
-// app.use('/users', users);
 require('./app/routes/microsoft')(app);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
