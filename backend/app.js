0a9be9856e5a47df0a3d652ff996f53b50a7adbf
kuminin <kumin.in@hotmail.com>
2016-10-22 02:47:04 -0700

    Required the microsoft.js route

--- backend/app.js
+++ backend/app.js
@@ -10,6 +10,7 @@ var users = require('./routes/users');
 
 var app = express();
 
+require('./app/models/db.js');
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -22,9 +23,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
-app.use('./app/routes/microsoft');
+// app.use('/', routes);
+// app.use('/users', users);
+require('./app/routes/microsoft')(app);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
