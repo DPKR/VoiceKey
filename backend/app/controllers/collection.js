7b000e859a80ee24d37b24da875876970b69fe71
Paul Kim <Paul Kim>
2016-10-23 06:28:04 -0700

    Finished documentation for delete collection routing

--- backend/app/controllers/collection.js
+++ backend/app/controllers/collection.js
@@ -61,14 +61,17 @@ module.exports.createNewCollection = (req, res) => {
             if (err) {
                 res.status(500).json(err);
             } else {
+                // Finds all collections that match with request name
                 var collection = user.collections.find((collection) => {
                     return collection.name == collectionName;
                 });
+                // if there were no matches, a new Collection object can be added
                 if (!collection) {
                     var newCollection = new Collection({
                         'user': user._id,
                         'name': collectionName
                     });
+                    // updates changes made to the collections context
                     user.collections.push(newCollection._id);
                     newCollection.save((err, collection) => {
                         if (err) {
@@ -83,6 +86,7 @@ module.exports.createNewCollection = (req, res) => {
                             });
                         }
                     });
+                // Cannot create a new Colleciton object if collection already exists
                 } else {
                     res.status(409).json({'error':'Collection already exist.'});
                 }
@@ -93,6 +97,7 @@ module.exports.createNewCollection = (req, res) => {
 
 module.exports.updateCollection = (req, res) => {
     if (req.decoded) {
+        // Takes request bodies and ensures that the values are valid inputs
         var collectionName = req.body.collectionName;
         var newCollectionName = req.body.newCollectionName;
         var userID = req.decoded._doc.Microsoft.id;
@@ -104,6 +109,8 @@ module.exports.updateCollection = (req, res) => {
             res.status(400).json({'error':'newCollectionName required in body'});
             return;
         }
+        // Searches all users according to the MicrosoftID/ userID field and
+        // populates the collections fields when a match is found
         User
         .findOne({'Microsoft.id':userID})
         .populate('collections')
@@ -111,12 +118,17 @@ module.exports.updateCollection = (req, res) => {
             if (err) {
                 res.status(500).json(err);
             } else {
+                // Attempts to grab the current collection query and the "new"
+                // collection query.
                 var collection = user.collections.find((collection) => {
                     return collection.name == collectionName;
                 });
                 var otherCollection = user.collections.find((collection) => {
                     return collection.name == newCollectionName;
                 });
+                // Makes sure that the collection information update is valid
+                // and that the corresponding collections exist before making
+                // saving the changes made.
                 if (!collection) {
                     res.status(404).json({'error':'Collection not found'});
                 } else if (otherCollection) {
@@ -144,6 +156,8 @@ module.exports.deleteCollection = (req, res) => {
             res.status(400).json({'error':'collectionName required from body'});
             return;
         }
+        // Searches all users according to the MicrosoftID/ userID field and
+        // populates the collections fields when a match is found
         User
         .findOne({'Microsoft.id':userID})
         .populate('collections')
@@ -151,23 +165,29 @@ module.exports.deleteCollection = (req, res) => {
             if (err) {
                 res.status(500).json(err);
             } else {
+                // Searches user's collections and returns a failing signal if
+                // there are no collections to delete
                 var thisCollection = user.collections.find((collection) => {
                     return collection.name == collectionName;
                 });
                 if (!thisCollection) {
                     res.status(404).json({'error':'No such collection exists'});
                 } else {
+                    // Search for index of collection to delete and remove it from
+                    // the list of user's collection list
                     var collectionID = mongoose.Types.ObjectId(thisCollection._id);
                     user.collections.splice(user.collections.indexOf(collectionID), 1);
                     user.save((err, user) => {
                         if (err) {
                             res.status(500).json(err);
                         } else {
+                            // Search for position of collection to delete
                             var listOfPasswords;
                             Collection.where().findOneAndRemove({'_id':collectionID}, (err, collection) => {
                                 if (err) {
                                     res.status(500).json(err);
                                 } else {
+                                    // Remove and delete passwords from collection to delete
                                     collection.passwords.forEach((password) => {
                                         var passID = mongoose.Types.ObjectId(password);
                                         Password.findOneAndRemove({'_id':password}, (err, password) => {
